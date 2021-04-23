402c0bb0ffae1b19453800a812b91aa0b0967dbd
Kshitij Kumar <kshitij2212@gmail.com>
2021-04-24 02:16:44 +0530

    two simple endpoints created

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,8 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
@@ -9,3 +11,28 @@ class DemoApplication
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
+
+@RestController
+class DemoController {
+
+	@GetMapping("/")
+	fun sendHello() : String = "Hello world!"
+
+	@GetMapping("/allVlogs")
+	fun sendVlogDetails() : List<Vlogs> {
+		return listOf(
+			Vlogs(1L, "Title1", "Kshitij", 1),
+			Vlogs(2L, "Title2", "Anushka", 2),
+			Vlogs(3L, "Title3", "Ankit", 3),
+			Vlogs(4L, "Title4", "Aashish", 4),
+			Vlogs(5L,creatorId =  5)
+		)
+	}
+}
+
+data class Vlogs(
+	val vlogId: Long,
+	val title: String = "Unnamed",
+	val creatorName: String = "Unnamed",
+	val creatorId: Long
+)
